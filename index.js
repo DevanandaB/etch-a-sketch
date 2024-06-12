84deceff2713c937fdee733f1913b0ed640c5105
Devananda Binu <deva31807@gmail.com>
2024-06-12 16:18:00 +0100

    Changes the grid function

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
+// const container = document.querySelector(".container");
+
+// function grid(num) {
+//     for(let i = 0; i < num; i++) {
+//         let div = document.createElement("div");
+//         div.style.border = "1px solid";
+//         div.style.width = "100%";
+//         div.style.height = "100%";
+//         container.appendChild(div);
+//     }
+// };
+
+// grid(16);
+
 const container = document.querySelector(".container");
+const hori = document.querySelector(".hori");
+const vert = document.querySelector(".vert");
 
 function grid(num) {
     for(let i = 0; i < num; i++) {
-    let div = document.createElement("div");
-    div.style.width = "50px";
-    div.style.height = "50px";
-    div.style.border = "1px solid";
-    container.appendChild(div);
-};
+        let div = document.createElement("div");
+        div.style.border = "1px solid";
+        div.style.width = "100%";
+        div.style.height = "100%";
+        hori.appendChild(div);
+    }
+    for(let i = 0; i < num; i++) {
+        let div2 = document.createElement("div");
+        div2.style.border = "1px solid";
+        div2.style.width = "100%";
+        div2.style.height = "100%";
+        vert.appendChild(div2);
+    }
 };
 
 grid(16);
