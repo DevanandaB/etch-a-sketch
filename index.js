e739b4bad0288810e08b4e3aeb759b85a72c09d8
Devananda Binu <deva31807@gmail.com>
2024-06-14 17:50:40 +0100

    Added a promptUser() and enabled hovering

--- index.js
+++ index.js
@@ -3,20 +3,38 @@ const container = document.querySelector(".container");
 function grid(num) {
     for(let i = 0; i < num; i++) {
         let row = document.createElement("div");
-        row.style.border = "1px solid black";
+        // row.style.border = "1px solid black";
         row.style.width = "100%";
         row.className = "row";
         container.appendChild(row); 
         for(let j = 0; j < num; j++) {
             let column = document.createElement("div");
-            column.style.border = "1px solid black";
+            column.style.border = "1px solid grey";
             column.style.width = "100%";
             column.style.height = "100%";
             column.className = "column";
+            function mouseOver() {
+                column.style.backgroundColor = "#C3B1E1";
+            }
+            column.addEventListener("mouseover", mouseOver);
             row.appendChild(column);
     }
-    }   
-    
+    }  
 };
 
-grid(16);
+promptUser() 
+
+function promptUser() {
+    let gridNum = prompt("heyo choose a number between 1-100...");
+    parseInt(gridNum);
+    if(gridNum == "" || !(gridNum)) {
+        grid(16);
+    } else if(gridNum < 1 || gridNum > 100) {
+        alert("You can't choose that! :(");
+        grid(16);
+    } else {
+        grid(gridNum);
+    }
+};
+
+
